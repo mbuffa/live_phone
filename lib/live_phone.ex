073bc53d6837266ef32aa89b9e42671d50a85199
Maxime Buffa <mbuffa@users.noreply.github.com>
2025-10-24 20:41:24 +0200

    Fix

--- lib/live_phone.ex
+++ lib/live_phone.ex
@@ -70,7 +70,7 @@ defmodule LivePhone do
         data-masks={@masks}
         phx-target={@myself}
         phx-change="typing"
-        phx-debounce="300"
+        phx-debounce="blur"
         phx-blur="close"
       />
 
